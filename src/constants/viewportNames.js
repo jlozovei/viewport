4eee277aac8a7a272bab95b61f4368958a31cd93
jlozovei <jlozovei@gmail.com>
2021-08-14 22:14:28 -0300

    feat: add viewport names constant

---
+++ src/constants/viewportNames.js
@@ -0,0 +1,37 @@
+export const viewportNames = {
+  xs: {
+    name: 'Extra small',
+    minWidth: 0,
+    maxWidth: 414
+  },
+
+  sm: {
+    name: 'Small',
+    minWidth: 415,
+    maxWidth: 600
+  },
+
+  md: {
+    name: 'Medium',
+    minWidth: 601,
+    maxWidth: 800
+  },
+
+  lg: {
+    name: 'Large',
+    minWidth: 801,
+    maxWidth: 1024
+  },
+
+  xl: {
+    name: 'Extra large',
+    minWidth: 1024,
+    maxWidth: 1535
+  },
+
+  xxl: {
+    name: 'Double large',
+    minWidth: 1536,
+    maxWidth: null
+  }
+};
